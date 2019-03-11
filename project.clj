53fb73f3259a87c206af613f54911d0d7e01c643
Santiago de Ledesma <santiago.deledesma@healthtrustpg.com>
2019-03-11 09:06:06 -0500

    Updated artifact version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ledesmadani.clojure/data-collector "0.2.0-SNAPSHOT"
+(defproject com.ledesmadani.clojure/data-collector "0.3.0-SNAPSHOT"
   :description "Library to encapsulate a background data collector"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
